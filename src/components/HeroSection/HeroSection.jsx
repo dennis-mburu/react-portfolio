d421aa74e8f64ff52f0e6e5d08653f7364bda0be
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2024-02-15 18:57:01 +0300

    :art: add starter hero section

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import "./HeroSection.css";
+import HoverGlowButton from "../HoverGlowButton/HoverGlowButton";
 
 function HeroSection() {
   return (
-    <div>HeroSection</div>
-  )
+    <div className="hero-section">
+      <div className="hero-text">
+        <p className="lead-text">
+          Hey, I'm Dennis
+          <br />
+          <span className="large-text">
+            Full Stack <br />
+            Developer
+          </span>
+        </p>
+        <p className="description">
+          Experienced developer passionate about creating seamless digital
+          experiences and specializing in crafting innovative solutions that
+          empower businesses and delight users.
+        </p>
+        <p className="description">
+          From building intuitive user interfaces to architecting scalable
+          backend systems, I bring creativity, precision, and expertise to every
+          project.
+        </p>
+        <p className="description">
+          Let's collaborate to build transformative web applications that make a
+          lasting impact.
+        </p>
+        <div className="btn-container">
+          <HoverGlowButton />
+        </div>
+      </div>
+      <div className="hero-image">
+        <img src="/images/img1.png" alt="profile pic" />
+      </div>
+    </div>
+  );
 }
 
-export default HeroSection
+export default HeroSection;
---
+++ src/components/HoverGlowButton/HoverGlowButton.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./HoverGlowButton.css";
+
+function HoverGlowButton() {
+  return (
+    <button class="glow-on-hover" type="button">
+      CLICK ME!
+    </button>
+  );
+}
+
+export default HoverGlowButton;
