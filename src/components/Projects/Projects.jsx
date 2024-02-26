0ac0274ab61786291d4e8606a93ca9e5b5ecbd30
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2024-02-26 12:02:20 +0300

    :art: map test projects ina grid display

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,30 +1,39 @@
 import React, { useRef, useEffect } from "react";
 import "./Projects.css";
+import profile from "../../profile";
+
 function Projects() {
   const imgRef = useRef(null);
 
-  useEffect(() => {
-    const imgHeight = imgRef.current.clientHeight;
-    const projectCard = document.querySelector(".project-card");
-    if (projectCard) {
-      projectCard.style.height = `${imgHeight}px`;
-    }
-  }, []);
+  // useEffect(() => {
+  //   const imgHeight = imgRef.current.clientHeight;
+  //   const projectCard = document.querySelector(".project-card");
+  //   if (projectCard) {
+  //     projectCard.style.height = `${imgHeight}px`;
+  //   }
+  // }, []);
   return (
     <div className="projects-section">
       <h1>Projects</h1>
-      <div className="project-card">
-        <div className="card-inner">
-          <div className="card-front">
-            <img ref={imgRef} src="/images/emall-v2.png" alt="screenshot" />
-          </div>
-          <div className="card-back">
-            <p>
-              A fully functional e-commerce platform built using the MERN stack
-              and Redux toolkit.
-            </p>
-          </div>
-        </div>
+      <div className="project-grid">
+        {profile.projects.map((project) => (
+          <>
+            <div className="project-card" key={project.name}>
+              <div className="card-inner">
+                <div className="card-front">
+                  <img ref={imgRef} src={project.image} alt="screenshot" />
+                </div>
+                <div className="card-back">
+                  <p>
+                    {/* A fully functional e-commerce platform built using the MERN
+                  stack and Redux toolkit. */}
+                    {project.description}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </>
+        ))}
       </div>
     </div>
   );
